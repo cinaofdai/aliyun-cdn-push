da1250a03b3c01b0c7fabe5cd4ff42cd38bbd66f
dailinlin <dailinlin@7477.com>
2017-06-20 17:53:56 +0800

    去除监测代码

--- tp-cdn-push/Application/Admin/Conf/config.php
+++ tp-cdn-push/Application/Admin/Conf/config.php
@@ -2,7 +2,7 @@
 return array(
 	//'配置项'=>'配置值'
     'TMPL_PARSE_STRING'  =>array(
-        '__PUBLIC__' => '/Public/Admin', // 更改默认的/Public 替换规则
+        '__PUBLIC__' => '/Public/admin', // 更改默认的/Public 替换规则
     ),
 
     //使用布局
